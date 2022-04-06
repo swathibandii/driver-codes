14f9dda4e5fd35c2a5b70ae8fc738da99fc6dccc
swathibandii <103120954+swathibandii@users.noreply.github.com>
2022-04-06 18:28:46 +0530

    special.c

--- numbers/cpp-driver-code.cpp
+++ numbers/cpp-driver-code.cpp
@@ -1,28 +1,22 @@
 #include <iostream>
-
-#include<bits/stdc++.h>
-using namespace std;
-
-string solve (int n) {
-   // Your code goes here
-   // n is the given input
-   return "Special";
-}
-
-// The main function reads the input in the required format.
-// Just complete the solve function above.
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int T;
-    cin >> T;
-    for(int t_i = 0; t_i < T; t_i++)
-    {
-        int n;
-        cin >> n;
-        string out_;
-        out_ = solve(n);
-        cout << out_;
-        cout << "\n";
-    }
-}
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   int n,i,count;
+   clrscr();
+   printf("enter number");
+   scanf("%i",&n);
+   i=1;
+   while(i<=n)
+      if(n%i==2||n%i==7)
+   {
+      printf("special):
+             i++;
+             }
+             else
+             {
+             printf("Regular):
+                    }
+                    }
+                    }
